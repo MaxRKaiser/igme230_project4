c135d911102eb3028ac8d264bd94144a8b990710
MaxRKaiser <maxrkaiser18@gmail.com>
2017-05-15 17:07:19 -0400

    Index page mostly complete
    Added javascript functionality and main content of index page. Images will need to be updated.

---
+++ SVG.js
@@ -0,0 +1,3 @@
+function grow() { alert('Grow and shrink a circle through the color spectrum'); }
+function budding() { alert('Move around and bud off copies'); }
+function cluster() { alert('randomly generate moving objects'); }
